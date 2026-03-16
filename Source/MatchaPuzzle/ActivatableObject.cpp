a76006452fd873db10eea086c4dcc90e6e22a60a
Valentina <vliberonazuniga@gmail.com>
2026-03-16 20:21:38 -0300

    feat: Enhance ActivatableObject with activation state management and child component handling

--- Source/MatchaPuzzle/ActivatableObject.cpp
+++ Source/MatchaPuzzle/ActivatableObject.cpp
@@ -2,6 +2,8 @@
 
 #include "ActivatableObject.h"
 #include "Components/StaticMeshComponent.h"
+#include "Components/SceneComponent.h"
+#include "Components/PrimitiveComponent.h"
 
 AActivatableObject::AActivatableObject()
 {
@@ -28,12 +30,55 @@ void AActivatableObject::BeginPlay()
 
 void AActivatableObject::Activate_Implementation()
 {
-	SetActorHiddenInGame(false);
-	SetActorEnableCollision(true);
+	SetActivationState(true);
 }
 
 void AActivatableObject::Deactivate_Implementation()
 {
-	SetActorHiddenInGame(true);
-	SetActorEnableCollision(false);
+	SetActivationState(false);
+}
+
+void AActivatableObject::SetActivated(bool bActivated)
+{
+	SetActivationState(bActivated);
+}
+
+void AActivatableObject::SetActivationState(bool bActive)
+{
+	SetActorHiddenInGame(!bActive);
+	SetActorEnableCollision(bActive);
+
+	if (bAffectChildComponents)
+	{
+		const ECollisionEnabled::Type CollisionMode =
+			bActive ? ECollisionEnabled::QueryAndPhysics : ECollisionEnabled::NoCollision;
+
+		TArray<UActorComponent*> Components;
+		GetComponents(Components);
+		for (UActorComponent* Comp : Components)
+		{
+			if (USceneComponent* SceneComp = Cast<USceneComponent>(Comp))
+			{
+				SceneComp->SetHiddenInGame(!bActive, false);
+			}
+			if (UPrimitiveComponent* PrimComp = Cast<UPrimitiveComponent>(Comp))
+			{
+				PrimComp->SetCollisionEnabled(CollisionMode);
+			}
+		}
+	}
+
+	if (bAffectAttachedActors)
+	{
+		TArray<AActor*> AttachedActors;
+		GetAttachedActors(AttachedActors, /*bResetArray=*/true, bRecursiveAttachedActors);
+		for (AActor* Attached : AttachedActors)
+		{
+			if (Attached)
+			{
+				Attached->SetActorHiddenInGame(!bActive);
+				Attached->SetActorEnableCollision(bActive);
+			}
+		}
+	}
 }
